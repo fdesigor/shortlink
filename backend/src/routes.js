7ee4f0dded2cf5699d520689c676bfb4693e70bb
igor Santos <igorsantos@bee.com.br>
2021-05-17 11:25:08 -0300

    for deploy

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const routes = require("express").Router();
 const LinkController = require("./controllers/LinkController");
 
 routes.get('/', LinkController.index)
+
 routes.post('/', LinkController.store)
 routes.get('/:link', LinkController.redirect)
 routes.get('/:link/count', LinkController.count)
