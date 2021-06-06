71e10ab2fb4423ac7fd5809fc62ea1414d847909
Igor Fernandes <fdesigor@gmail.com>
2021-06-05 21:02:07 -0300

    Ajustes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,11 @@ const routes = require("./routes");
 require("dotenv/config");
 require("./database");
 
-const app = express()
-const port = 3000
+const app = express();
 
 // Configurações do Servidor
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.APP_PORT);
